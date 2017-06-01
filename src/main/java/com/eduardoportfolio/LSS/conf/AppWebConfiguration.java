db9425319cd76b8c83556ee63d37523b267d5e00
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-06-01 13:40:40 -0300

    comments

--- src/main/java/com/eduardoportfolio/LSS/conf/AppWebConfiguration.java
+++ src/main/java/com/eduardoportfolio/LSS/conf/AppWebConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 //The principal objective of this class is show for the SpringMvc Servlet what classes should be read and loaded.
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	
+	//Show to Spring that the return from this method have to be registered as an object managed by the container.
 	@Bean
 	//Teach Spring where to look for the pages, where the pages can be found.
 	public InternalResourceViewResolver internalResourceViewResolver (){
