205c7f21c23a2bc87bd57c68d901aae0a067f665
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-06-01 12:44:30 -0300

    AppWebConfiguration class created

---
+++ src/main/java/com/eduardoportfolio/LSS/conf/AppWebConfiguration.java
@@ -0,0 +1,7 @@
+package com.eduardoportfolio.LSS.conf;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class AppWebConfiguration extends WebMvcConfigurerAdapter {
+
+}
