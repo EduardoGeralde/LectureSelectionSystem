61ca3413bd002ed55220da7106e685744becd52a
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-06-01 14:23:52 -0300

    EventController class created

---
+++ src/main/java/com/eduardoportfolio/LSS/controllers/EventController.java
@@ -0,0 +1,17 @@
+package com.eduardoportfolio.LSS.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//Tells that this class is effectively the responsible to meet requests from a client
+@Controller
+public class EventController {
+	
+	//Binding (connection between a route and a method) Which method have to call for each URL
+	@RequestMapping("/")
+	public String index(){
+		return "homeLSS";
+	}
+
+	
+}
