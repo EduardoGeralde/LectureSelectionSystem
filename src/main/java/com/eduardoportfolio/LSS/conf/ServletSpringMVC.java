17dbdef782edeb5c439ac04cdba45d24a3b2ef08
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-06-01 10:13:07 -0300

    ServletSpringClass created, and the pattern / added in the getServletMappings method

---
+++ src/main/java/com/eduardoportfolio/LSS/conf/ServletSpringMVC.java
@@ -0,0 +1,24 @@
+package com.eduardoportfolio.LSS.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String [] {"/"};
+	}
+
+}
