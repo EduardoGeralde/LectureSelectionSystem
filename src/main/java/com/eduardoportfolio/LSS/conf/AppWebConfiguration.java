8f58b93547fe9f4f2ee14d12d7940f34ccb167b0
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-06-01 12:56:16 -0300

    Annotations in the class

--- src/main/java/com/eduardoportfolio/LSS/conf/AppWebConfiguration.java
+++ src/main/java/com/eduardoportfolio/LSS/conf/AppWebConfiguration.java
@@ -1,7 +1,11 @@
 package com.eduardoportfolio.LSS.conf;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@EnableWebMvc
+@ComponentScan(basePackageClasses={})
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 
 }
