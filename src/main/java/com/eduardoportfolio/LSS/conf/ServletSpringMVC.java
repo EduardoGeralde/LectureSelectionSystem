77218dd60a5f8209c4c0377f6fb7b332f84a3045
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-06-01 10:25:51 -0300

    comments

--- src/main/java/com/eduardoportfolio/LSS/conf/ServletSpringMVC.java
+++ src/main/java/com/eduardoportfolio/LSS/conf/ServletSpringMVC.java
@@ -2,6 +2,9 @@ package com.eduardoportfolio.LSS.conf;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+//Following the servlets specification, in web projects, is necessary to have at least one Filter or Servlet.
+//The Servlet responsible for process all incoming requests for Spring MVC is the DispatcherServlet.
+//We need it to everything starts working.
 public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
@@ -10,11 +13,13 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 		return null;
 	}
 
+	//Tells SpringMVC which controller have to be mapped, and which classes has to be loaded.
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		return new Class[] {AppWebConfiguration.class};
 	}
 
+	//Indicates what is the pattern of address that will be delegated to the SpringMVC servlet.
 	@Override
 	protected String[] getServletMappings() {
 		return new String [] {"/"};
