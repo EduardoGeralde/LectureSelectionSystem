05988d1c61b18246dfa5e48e5fbea70b1258e0d3
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-06-03 08:41:34 -0300

    imported the user class

--- src/main/java/com/eduardoportfolio/LSS/controllers/UserController.java
+++ src/main/java/com/eduardoportfolio/LSS/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.eduardoportfolio.LSS.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.eduardo.LSS.models.User;
+
 @Controller
 public class UserController {
 	
