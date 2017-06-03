446ed2531b9e2f6cb1dfad4493cda10ed41f9ab8
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-06-03 08:41:15 -0300

    imported the lecture class

--- src/main/java/com/eduardoportfolio/LSS/controllers/LecturesController.java
+++ src/main/java/com/eduardoportfolio/LSS/controllers/LecturesController.java
@@ -3,6 +3,8 @@ package com.eduardoportfolio.LSS.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.eduardo.LSS.models.Lecture;
+
 @Controller
 public class LecturesController {
 	
