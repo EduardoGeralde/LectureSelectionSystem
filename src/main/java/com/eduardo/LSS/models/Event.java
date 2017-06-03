305ea075c609e13ceadfd6bd3565c65e00e5c4dd
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-06-03 17:13:49 -0300

    persistence annotations and attributes settings annotations

--- src/main/java/com/eduardo/LSS/models/Event.java
+++ src/main/java/com/eduardo/LSS/models/Event.java
@@ -4,10 +4,24 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+//Indicates that the class have to be represented by a table in DB.
+@Entity
 public class Event {
 	
+	//Indicates that the attribute is a primary key.
+	@Id
+	//Indicates how the primary key will be generated.
+	@GeneratedValue ( strategy = GenerationType.IDENTITY)
 	private Integer eventId;
 	private String eventName;
+	//Indicates that it will be saved like a CLOB or BLOB in the DB.
+	@Lob
 	private String eventDescription;
 	private String eventSite;
 	private String eventOrganizer;
