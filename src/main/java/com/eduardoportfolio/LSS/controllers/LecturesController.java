1ced47f14bc3d4303b7d27e75b5a5cbdf0cfb84b
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-06-04 12:18:23 -0300

    save method implanted

--- src/main/java/com/eduardoportfolio/LSS/controllers/LecturesController.java
+++ src/main/java/com/eduardoportfolio/LSS/controllers/LecturesController.java
@@ -2,9 +2,11 @@ package com.eduardoportfolio.LSS.controllers;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.eduardoportfolio.LSS.dao.LectureDao;
 import com.eduardoportfolio.LSS.models.Lecture;
 
 /**
@@ -20,9 +22,14 @@ import com.eduardoportfolio.LSS.models.Lecture;
 @Transactional
 public class LecturesController {
 	
+	//Responsible to indicates the injection points inside the class
+	@Autowired
+	LectureDao lectureDao = new LectureDao();
+	
 	@RequestMapping ("/lectures")
 	public String save (Lecture lecture) {
-		System.out.println("Registering the Lecture");
+		lectureDao.save(lecture);
+		System.out.println("Saving Lecture" + lecture);
 		return "lectures/ok";
 	}
 }
