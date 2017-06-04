91866f31bdf529be12d9d6fa5435af5a9dae8324
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-06-04 09:29:06 -0300

    annotations

---
+++ src/main/java/com/eduardoportfolio/LSS/dao/EventDao.java
@@ -0,0 +1,21 @@
+package com.eduardoportfolio.LSS.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.eduardoportfolio.LSS.models.Event;
+
+//Indicates that this class besides being managed for the Spring, it is responsible for the data access.
+@Repository
+public class EventDao {
+	
+	//Used to inject Entity Manager
+	@PersistenceContext
+	private EntityManager manager;
+	
+	public void save (Event event){
+		manager.persist(event);
+	}
+}
