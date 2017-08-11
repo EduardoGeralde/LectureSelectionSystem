08fa6a9e1f1f717da8024717c1fc90639517f5fb
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-08-11 20:23:57 -0300

    same

--- src/main/java/com/eduardoportfolio/LSS/controllers/HomeController.java
+++ src/main/java/com/eduardoportfolio/LSS/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * 
  * @author Eduardo Geralde Neto
  * 
- * 
+ * .
  */
 
 //Tells that this class is effectively the responsible to meet requests from a client
