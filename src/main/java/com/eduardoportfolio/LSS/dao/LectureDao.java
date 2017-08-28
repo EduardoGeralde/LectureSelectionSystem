90f3d11451a1774b0dde798c06bc8be339b9d6aa
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-08-27 23:26:44 -0300

    methods added

--- src/main/java/com/eduardoportfolio/LSS/dao/LectureDao.java
+++ src/main/java/com/eduardoportfolio/LSS/dao/LectureDao.java
@@ -1,10 +1,13 @@
 package com.eduardoportfolio.LSS.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 
+import com.eduardoportfolio.LSS.models.Event;
 import com.eduardoportfolio.LSS.models.Lecture;
 
 /**
@@ -24,5 +27,13 @@ public class LectureDao {
 	
 	public void save (Lecture lecture){
 		manager.persist(lecture);
+	}
+	
+	public void delete (Lecture lecture){
+		manager.remove(lecture);
+	}
+	
+	public List<Lecture> list(){
+		return manager.createQuery("select * from Lecture", Lecture.class).getResultList();
 	}
 }
